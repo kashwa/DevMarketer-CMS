2a25137b842a7586789903633644723fef3e4c85
kashwa <kashwa80@gmail.com>
2018-09-09 14:28:11 +0200

    modification-Buefy vs Bootstrap

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -2,15 +2,12 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
+ * We'll load jQuery. This code may be 
+ * modified to fit the specific needs of your application.
  */
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
 } catch (e) {}
 
 /**
