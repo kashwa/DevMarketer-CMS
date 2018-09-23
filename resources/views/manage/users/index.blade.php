89fc10b1225a99e6a32e1126fa9831088aeecc2f
kashwa <kashwa80@gmail.com>
2018-09-23 19:02:34 +0200

    add button with route to View user details

--- resources/views/manage/users/index.blade.php
+++ resources/views/manage/users/index.blade.php
@@ -31,7 +31,7 @@
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>{{ $user->created_at->toFormattedDateString() }}</td>
-                                <td><a class="button is-outlined" href="{{route('users.edit', $user->id)}}">Edit</a></td>
+                                <td><a class="button is-outlined" href="{{route('users.edit', $user->id)}}">Edit</a> <a class="button is-primary m-l-5" href="{{route('users.show', $user->id)}}">View</a></td>
                             </tr>
                             @endforeach
                         </tbody>
