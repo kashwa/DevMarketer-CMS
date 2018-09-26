b9803b6919710a14532c932e765a8a565686bbb3
kashwa <kashwa80@gmail.com>
2018-09-26 18:14:29 +0200

    Adding sliding admin nested menus

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -2,16 +2,16 @@
     <aside class="menu m-t-30 m-l-10">
         <p class="menu-label">General</p>
         <ul class="menu-list">
-            <li class="side-menu-item"><a href=" {{route('manage.dashboard')}} ">Dashboard</a></li>
+            <li class="side-menu-item"><a class="is-clk" href=" {{route('manage.dashboard')}} ">Dashboard</a></li>
         </ul>
 
         <p class="menu-label">Administration</p>
 
         <ul class="menu-list">
-            <li class="side-menu-item"><a href="{{route('users.index')}}">Manage Users</a></li>
+            <li class="side-menu-item"><a class="is-clk" href="{{route('users.index')}}">Manage Users</a></li>
             <li class="side-menu-item">
-                <a href="{{ route('permissions.index')}}">Roles &amp; Permissions</a>
-                <ul>
+                <a class="has-submenu">Roles &amp; Permissions</a>
+                <ul class="submenu">
                     <li><a href="{{route('roles.index')}}">Roles</a></li>
                     <li><a href="{{route('permissions.index')}}">Permissions</a></li>
                 </ul>
