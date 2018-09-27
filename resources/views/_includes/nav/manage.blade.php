36746a3d376542cfeec839da5978b695702e0496
kashwa <kashwa80@gmail.com>
2018-09-27 16:30:26 +0200

    styling Active elements in Side-menu

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -2,18 +2,18 @@
     <aside class="menu m-t-30 m-l-10">
         <p class="menu-label">General</p>
         <ul class="menu-list">
-            <li class="side-menu-item"><a class="is-clk" href=" {{route('manage.dashboard')}} ">Dashboard</a></li>
+            <li class="side-menu-item"><a class="{{Nav::isRoute('manage.dashboard')}}" href=" {{route('manage.dashboard')}} ">Dashboard</a></li>
         </ul>
 
         <p class="menu-label">Administration</p>
 
         <ul class="menu-list">
-            <li class="side-menu-item"><a class="is-clk" href="{{route('users.index')}}">Manage Users</a></li>
+            <li class="side-menu-item"><a class="{{Nav::isResource('users')}}" href="{{route('users.index')}}">Manage Users</a></li>
             <li class="side-menu-item">
-                <a class="has-submenu">Roles &amp; Permissions</a>
+                <a class="has-submenu {{Nav::hasSegment(['roles', 'permissions'], 2 )}}">Roles &amp; Permissions</a>
                 <ul class="submenu">
-                    <li><a href="{{route('roles.index')}}">Roles</a></li>
-                    <li><a href="{{route('permissions.index')}}">Permissions</a></li>
+                    <li><a class="{{Nav::isResource('roles')}}" href="{{route('roles.index')}}">Roles</a></li>
+                    <li><a class="{{Nav::isResource('permissions')}}" href="{{route('permissions.index')}}">Permissions</a></li>
                 </ul>
             </li>
         </ul>
