60cdb1f116911223a818a1f6e363d79e43311b5d
kashwa <kashwa80@gmail.com>
2018-09-28 17:40:52 +0200

    generate post controller & add link in side-menu

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -1,8 +1,22 @@
 <div class="side-menu" id="admin-side-menu">
     <aside class="menu m-t-30 m-l-10">
         <p class="menu-label">General</p>
+        
         <ul class="menu-list">
             <li class="side-menu-item"><a class="{{Nav::isRoute('manage.dashboard')}}" href=" {{route('manage.dashboard')}} ">Dashboard</a></li>
+        </ul>
+
+        <p class="menu-label">Content</p>
+
+        <ul class="menu-list">
+            <li class="side-menu-item"><a class="{{Nav::isResource('posts', 2)}}" href="{{route('posts.index')}}">Blog Posts</a></li>
+            {{-- <li class="side-menu-item">
+                <a class="has-submenu {{Nav::hasSegment(['roles', 'permissions'], 2 )}}">Roles &amp; Permissions</a>
+                <ul class="submenu">
+                    <li><a class="{{Nav::isResource('roles')}}" href="{{route('roles.index')}}">Roles</a></li>
+                    <li><a class="{{Nav::isResource('permissions')}}" href="{{route('permissions.index')}}">Permissions</a></li>
+                </ul>
+            </li> --}}
         </ul>
 
         <p class="menu-label">Administration</p>
