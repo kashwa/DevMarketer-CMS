bbcb879ab9237e57d34fa2fa5cbe265effd066d2
kashwa <kashwa80@gmail.com>
2018-09-29 17:34:43 +0200

    create post index

---
+++ resources/views/manage/posts/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.manage') 
+
+@section('content')
+    <div class="flex-container">
+        <div class="columns m-t-10">
+            <div class="column">
+                <h1 class="title">Posts index page</h1>
+            </div>
+            <div class="column">
+                <a href="{{route('posts.create')}}" class="button is-primary is-pulled-right"><i class="fa fa-user-plus m-r-10"></i> Create New Post</a>
+            </div>
+        </div>
+        <hr class="m-t-0">
+
+
+
+    </div> {{-- End of .flex-container --}}
+@endsection
