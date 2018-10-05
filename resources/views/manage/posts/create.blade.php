2b71d7dcec4d5ba5335de5bdbb8f5c5e1b451ca5
kashwa <kashwa80@gmail.com>
2018-10-05 18:53:15 +0200

    Get api_token from Blade template

--- resources/views/manage/posts/create.blade.php
+++ resources/views/manage/posts/create.blade.php
@@ -72,7 +72,8 @@
             el: '#app',
             data: {
                 title: '',
-                slug: ''
+                slug: '',
+                api_token: '{{Auth::user()->api_token}}'
             },
             methods: {
                 updateSlug: function (val) {
