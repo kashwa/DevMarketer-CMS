54642257014b604aadb180d41bab41612ee14b56
kashwa <kashwa80@gmail.com>
2018-10-13 15:20:40 +0200

    Styled Posts page for each user and for Own user.

--- resources/views/manage/dashboard.blade.php
+++ resources/views/manage/dashboard.blade.php
@@ -23,41 +23,43 @@
             <img src="https://bulma.io/images/placeholders/128x128.png">
           </p>
         </figure>
-        <div class="media-content">
+        <div class="media-content m-b-20">
           <div class="content">
             <p>
-              <strong>{{$post->user->name}}</strong> <small>@johnsmith</small> <small>31m</small>
+              <strong>{{$post->user->name}}</strong> <small>({{$post->user->email}})</small> <small>{{$post->created_at}}</small>
               <br>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+              {!! $post->content !!}
             </p>
           </div>
           <nav class="level is-mobile">
             <div class="level-left">
               <a class="level-item">
-                <span class="icon is-small"><i class="fa fa-reply"></i></span>
+                <span class="icon is-small">({{$post->comment_count}})<i class="fa fa-comment m-l-5"></i></span>
               </a>
               <a class="level-item">
-                <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fa fa-heart"></i></span>
+                <span class="icon is-small"><i class="fa fa-heart m-l-50"></i></span>
               </a>
             </div>
           </nav>
-        </div>
-        <div class="media-right">
-          <button class="delete"></button>
         </div>
       </article>
     @endforeach
     </div>
 
     <div class="column">
-      <label for="slug" class="label">Slug:</label>
-      <div class="field">
-        @foreach ($posts as $post)
-          {{$post->title}}
-        @endforeach
+      <div class="container is-fluid">
+        <div class="notification">
+          <label for="slug" class="label">Slug:</label>
+          <div class="field">
+            @foreach ($posts as $post)
+              <ul>
+                <li>
+                  <a href="#">{{$post->slug}}</a>
+                </li>
+              </ul>
+            @endforeach
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- resources/views/manage/posts/index.blade.php
+++ resources/views/manage/posts/index.blade.php
@@ -11,14 +11,48 @@
             </div>
         </div>
         <hr class="m-t-0">
-
+        <section class="hero is-success m-b-20">
+          <div class="hero-body">
+            <div class="container has-text-centered">
+              <h1 class="title">
+                DevMarketer - Posts Index
+              </h1>
+              <h2 class="subtitle">
+                Here, you'll see All Posts of <strong>Yours</strong> ;)
+              </h2>
+            </div>
+          </div>
+        </section>
         {{-- how to access each user's post --}}
         @foreach ($posts as $post)
 
           @if (Auth::user()->id == $post->author_id)
-            {{'Title : '.$post->title}}<br><br><br>
-            {{'content : '.$post->content}}<br><br><br>
-            {{'Author ID: '.$post->author_id}}
+            <article class="media">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <img src="https://bulma.io/images/placeholders/128x128.png">
+                </p>
+              </figure>
+              <div class="media-content m-b-20">
+                <div class="content">
+                  <p>
+                    <strong>{{$post->user->name}}</strong> <small>({{$post->user->email}})</small> <small>{{$post->created_at}}</small>
+                    <br>
+                    {!! $post->content !!}
+                  </p>
+                </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item">
+                      <span class="icon is-small">({{$post->comment_count}})<i class="fa fa-comment m-l-5"></i></span>
+                    </a>
+                    <a class="level-item">
+                      <span class="icon is-small"><i class="fa fa-heart m-l-50"></i></span>
+                    </a>
+                  </div>
+                </nav>
+              </div>
+            </article>
           @endif
 
         @endforeach
