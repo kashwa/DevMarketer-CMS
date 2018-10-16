6b8b9c13f8d24e3fc22791b4194f7ba0a9df6c35
kashwa <kashwa80@gmail.com>
2018-10-16 14:07:28 +0200

    styling homepage and adding Youtube Videos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<div class="flex-container" id="flexcontainer">
+    <div class="columns mdlcol">
+        <div class="column">
+            <div class="middle">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/rzxNpfiLHSg" frameborder="0" allow="autoplay; encrypted-media"
+                    allowfullscreen></iframe>
             </div>
+        </div>
+        <div class="column">
+            <article class="media">
+                <div class="media-content">
+                    <div class="content" style="background-color: #ccc; padding: 15px;">
+                        <p>
+                            <strong class="m-r-5">Ja_curtis</strong> <small>@jacurtis</small>
+                            <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor
+                            vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+
+    <div class="columns mdlcol m-t-40">
+        <div class="column">
+            <div class="middle">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/-FMecyZs5Cg" frameborder="0" allow="autoplay; encrypted-media"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="column">
+            <article class="media">
+                <div class="media-content">
+                    <div class="content" style="background-color: #ccc; padding: 15px;">
+                        <p>
+                            <strong class="m-r-5">devmarketer</strong> <small>@devmarketer.io</small>
+                            <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque
+                            tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                        </p>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+
+    <div class="columns mdlcol m-t-40">
+        <div class="column">
+            <div class="middle">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/r9HdJ8P6GQI" frameborder="0" allow="autoplay; encrypted-media"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="column">
+            <article class="media">
+                <div class="media-content">
+                    <div class="content" style="background-color: #ccc; padding: 15px;">
+                        <p>
+                            <strong class="m-r-5">max</strong> <small>@academind</small>
+                            <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque
+                            tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                        </p>
+                    </div>
+                </div>
+            </article>
         </div>
     </div>
 </div>
